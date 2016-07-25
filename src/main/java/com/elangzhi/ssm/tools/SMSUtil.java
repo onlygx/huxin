fa4f257ef04b502aeb7f9916cef191ada7163c76
onlygx <only_gx@126.com>
2016-07-25 15:17:35 +0800

    company

--- src/main/java/com/elangzhi/ssm/tools/SMSUtil.java
+++ src/main/java/com/elangzhi/ssm/tools/SMSUtil.java
@@ -11,6 +11,12 @@ import java.net.URLEncoder;
  */
 public class SMSUtil {
 
+    private static String apikey = "612d1a51fb80fefa9caece30e46a0eb7";
+    private static String username = "qhgh";
+    private static String password_md5 = "46c048cadc663f645b07c5dfa2328995";
+    private static String encode = "UTF-8";
+
+
     /**
      * 发送短信
      * @param phone 手机号
@@ -30,18 +36,7 @@ public class SMSUtil {
         //新建一个StringBuffer链接
         StringBuffer buffer = new StringBuffer();
 
-
-        //String encode = "GBK"; //页面编码和短信内容编码为GBK。重要说明：如提交短信后收到乱码，请将GBK改为UTF-8测试。如本程序页面为编码格式为：ASCII/GB2312/GBK则该处为GBK。如本页面编码为UTF-8或需要支持繁体，阿拉伯文等Unicode，请将此处写为：UTF-8
-
-        String encode = "UTF-8";
-
-        String username = "qhgh";  //用户名
-
-        String password_md5 = "46c048cadc663f645b07c5dfa2328995";  //密码 asdf1234a
-
         String mobile = phone;  //手机号,只发一个号码：13800000001。发多个号码：13800000001,13800000002,...N 。使用半角逗号分隔。
-
-        String apikey = "612d1a51fb80fefa9caece30e46a0eb7";  //apikey秘钥（请登录 http://m.5c.com.cn 短信平台-->账号管理-->我的信息 中复制apikey）
 
         try {
 
@@ -49,7 +44,14 @@ public class SMSUtil {
             String contentUrlEncode = URLEncoder.encode(content,encode);  //对短信内容做Urlencode编码操作。注意：如
 
             //把发送链接存入buffer中，如连接超时，可能是您服务器不支持域名解析，请将下面连接中的：【m.5c.com.cn】修改为IP：【115.28.23.78】
-            buffer.append("http://m.5c.com.cn/api/send/index.php?username="+username+"&password_md5="+password_md5+"&mobile="+mobile+"&apikey="+apikey+"&content="+contentUrlEncode+"&encode="+encode);
+            buffer.append("http://m.5c.com.cn/api/send/index.php?"
+                    +"username=" +username
+                    +"&password_md5=" +password_md5
+                    +"&mobile=" +mobile
+                    +"&apikey=" +apikey
+                    +"&content=" +contentUrlEncode
+                    +"&encode="+encode
+            );
 
             //System.out.println(buffer); //调试功能，输入完整的请求URL地址
 
